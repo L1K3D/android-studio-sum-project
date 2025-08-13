ee188b2d3db30a033a74bc03d7faa4a0ed6585f1
Heitor S. Ferreira <heitor.ferreira231203@gmail.com>
2025-08-12 22:41:12 -0300

    Feature: Adding first alterations on the app config | 12.08.2025

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Text>Digite o primeiro valor: </Text>
+      <TextInput
+      />
+      <TouchableOpacity>
+        <Text>Somar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
